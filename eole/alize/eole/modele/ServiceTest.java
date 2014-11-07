c97e4a303dc1f19d345ce3d22728b4b47f640786
saintocy <cyril.saintot@reseau.eseo.fr>
2014-11-07 08:13:25 +0100

    Ajout des fichiers

---
+++ eole/alize/eole/modele/ServiceTest.java
@@ -0,0 +1,10 @@
+package alize.eole.modele;
+
+import org.testng.annotations.Test;
+
+public class ServiceTest {
+  @Test
+  public void creerUnService() {
+	  System.out.println("Je suis ici ! ");
+  }
+}
---
+++ eole/src/main/java/alize/eole/modele/Phase.java
@@ -0,0 +1,17 @@
+package alize.eole.modele;
+
+import java.util.Date;
+
+/**
+ * @name Phase
+ * @author Cyril [CS]
+ * @date 23 oct. 2014
+ * @version 1
+ */
+public class Phase {
+
+	private Date debut;
+	private Date fin; 
+	private Date periodicite;
+	
+}
